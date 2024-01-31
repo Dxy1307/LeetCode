093e43d0c697250cb9f56a06df59c2748784feb9
Dxy1307 <ngducduy137@gmail.com>
2024-01-31 18:28:30 +0700

    start with problem 1.Two Sum

---
+++ Easy/prob1.cpp
@@ -0,0 +1,36 @@
+// Two Sum
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int temp = target - nums[i];
+            if (map.count(temp)) 
+                return {map[temp], i};
+            map[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);;
+    cin.tie(0);cout.tie(0);
+
+    vector<int> num = {2, 7, 11, 15};
+    int target = 9;
+    Solution so;
+    vector<int> rs = so.twoSum(num, target);
+    for(const auto &element : rs) {
+        cout << element << " ";
+    }
+    return 0;
+}
